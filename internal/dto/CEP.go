cdca9992870468d0b960cb90fc0f1ab5efd2b2b9
nagahshi <willian.empari@gmail.com>
2024-07-23 16:05:54 -0300

    feat: dto de entrada e saida de dados e transformer para CEP e weather

--- internal/dto/CEP.go
+++ internal/dto/CEP.go
@@ -4,4 +4,25 @@ type CEPInput struct {
 	CEP string
 }
 
-type CEPOutput struct{}
+type CEPOutput struct {
+	Logradouro string
+	Bairro     string
+	UF         string
+	CIDADE     string
+	Latitude   string
+	Longitude  string
+}
+
+// ToWeatherInput converts a CEPOutput to a WeatherInput
+func (c CEPOutput) ToWeatherInput() WeatherInput {
+	var weatherInput WeatherInput
+
+	weatherInput.Logradouro = c.Logradouro
+	weatherInput.Bairro = c.Bairro
+	weatherInput.UF = c.UF
+	weatherInput.CIDADE = c.CIDADE
+	weatherInput.Latitude = c.Latitude
+	weatherInput.Longitude = c.Longitude
+
+	return weatherInput
+}
--- internal/dto/Weather.go
+++ internal/dto/Weather.go
@@ -1,5 +1,16 @@
 package dto
 
-type WeatherInput struct{}
+type WeatherInput struct {
+	Logradouro string
+	Bairro     string
+	UF         string
+	CIDADE     string
+	Latitude   string
+	Longitude  string
+}
 
-type WeatherOutput struct{}
+type WeatherOutput struct {
+	C float64 `json:"celsius"`
+	F float64 `json:"fahrenheit"`
+	K float64 `json:"kelvin"`
+}
