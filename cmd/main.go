695cdd42601382b1eca3c861a71e38623c67a2e0
nagahshi <willian.empari@gmail.com>
2024-07-23 16:09:08 -0300

    chore: atualizando dependencias ajustando api de consultas e chaves

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	webSrv "github.com/nagahshi/pos_go_weather/internal/infra/web/server"
 	usecase "github.com/nagahshi/pos_go_weather/internal/useCase"
 
@@ -10,12 +13,18 @@ import (
 func main() {
 	webserver := webSrv.NewWebServer("8080")
 
-	healthyHandler := web.NewWeatherHandler(
+	key := os.Getenv("WEATHER_API_KEY")
+	if key == "" {
+		log.Fatal("chave de consulta [WEATHER_API_KEY] não encontrada")
+		return
+	}
+
+	weatherHandler := web.NewWeatherHandler(
 		*usecase.NewSearchCEPUseCase(),
-		*usecase.NewGetWeatherUseCase(),
+		*usecase.NewGetWeatherUseCase(key),
 	)
 
-	webserver.AddHandler("/cep/{cep}", healthyHandler.GetWeather)
+	webserver.AddHandler("/cep/{cep}", weatherHandler.GetWeather)
 
 	println("Starting web server on port:", ":8080")
 	webserver.Start()
--- go.mod
+++ go.mod
@@ -3,3 +3,8 @@ module github.com/nagahshi/pos_go_weather
 go 1.20
 
 require github.com/go-chi/chi/v5 v5.1.0
+
+require (
+	github.com/go-chi/chi v1.5.5 // indirect
+	github.com/valyala/fastjson v1.6.4 // indirect
+)
